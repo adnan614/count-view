3444212b8d241922a07d497ad142b1ed7afc9580
shourov-sorkar <18103142@iubat.edu>
2021-02-12 17:48:25 +0600

    :tada: add state and module css

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,32 +1,21 @@
-import react from 'react';
+import { useState } from "react";
+import style from "./app.module.css"
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const increment = (val = 1) => setCounter(counter + val);
+  const decrement = (val = 1) => setCounter(counter - val);
 
-
-class App extends react.Component{
-
-
-    state = {
-        count: 0
-    };
-
-    render()
-    {
-        return(
-
-            <div className="App">
-               <h1 style={{color: 'red'}}>Why do we need State? </h1>
-               <h1>Count: {this.state.count}</h1>
-               <button onClick={() => {
-                //    this.count++;
-                this.setState({count: this.state.count + 1});
-                   console.log('clicked ...', this.state.count);
-               }}>
-
-               Add +1
-               </button>
-            </div>
-        );
-    }
-}
+  return (
+    <div className={style.App}>
+      <h1>Counter : {counter}</h1>
+      <div style={{ display: "block" }}>
+        <button onClick={() => increment()}>Increment +1</button>
+        <button onClick={() => increment(5)}>Increment +5</button>
+        <button onClick={() => decrement()}>Decrement -1</button>
+        <button onClick={() => decrement(5)}>Decrement -5</button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
-
